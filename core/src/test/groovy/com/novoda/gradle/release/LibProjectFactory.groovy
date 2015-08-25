e30dd9d83b26e1652b28cb9ee2815ec32af12af3
Friedger Müffke <friedger@gmail.com>
2015-08-25 15:25:43 +0200

    separate test into several methods, use naming convention for demo app

--- core/src/test/groovy/com/novoda/gradle/release/LibProjectFactory.groovy
+++ core/src/test/groovy/com/novoda/gradle/release/LibProjectFactory.groovy
@@ -6,10 +6,10 @@ import org.gradle.testfixtures.ProjectBuilder
 final class LibProjectFactory {
 
     public static Project createFromFixture() {
-        Project project = ProjectBuilder.builder().withProjectDir(new File(TestBintrayRelease.FIXTURE_WORKING_DIR)).build()
+        Project project = ProjectBuilder.builder().withProjectDir(new File(TestConfiguration.FIXTURE_WORKING_DIR)).build()
         project.apply plugin: 'com.android.library'
         project.version = "1.0"
-        project.group = "com.novoda.gradle.release.demo"
+        project.group = "com.novoda.demo.gradle.release"
         project.android {
             compileSdkVersion 23
             buildToolsVersion '23.0.0'
@@ -19,12 +19,6 @@ final class LibProjectFactory {
                 versionName '1.0'
                 minSdkVersion 23
                 targetSdkVersion 23
-            }
-
-            buildTypes {
-                release {
-                    signingConfig signingConfigs.debug
-                }
             }
         }
 
--- core/src/test/groovy/com/novoda/gradle/release/TestConfiguration.groovy
+++ core/src/test/groovy/com/novoda/gradle/release/TestConfiguration.groovy
@@ -3,51 +3,64 @@ import com.jfrog.bintray.gradle.BintrayUploadTask
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.publish.maven.tasks.GenerateMavenPom
+import org.junit.After
 import org.junit.AfterClass
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 
 import static com.google.common.truth.Truth.assertThat
 
-class TestBintrayRelease {
+class TestConfiguration {
 
     // This is necessary because the IDE debugger and command line invocations have different working directories ಠ_ಠ
     private static final WORKING_DIR = System.getProperty("user.dir")
     private static final PATH_PREFIX = WORKING_DIR.endsWith("core") ? WORKING_DIR : "$WORKING_DIR/core"
 
     static final String FIXTURE_WORKING_DIR = "$PATH_PREFIX/src/test/fixtures/android_app"
+    static Project project
+
+    @BeforeClass
+    public static void setup() {
+        project = LibProjectFactory.createFromFixture()
+        ReleasePlugin plugin = new ReleasePlugin()
+        plugin.apply(project)
+        project.publish {
+            userOrg = 'novoda'
+            groupId = 'com.novoda.demo'
+            artifactId = "test"
+            version = project.version
+            description = 'Sample library'
+            dryRun = true
+        }
+        project.evaluate()
+
+    }
 
     @AfterClass
     static void tearDown() {
-        // I don't know why this gets generated, but toss it
+        project.buildDir.deleteDir();
+
+        // delete folders generated during project evaluation
         new File("${FIXTURE_WORKING_DIR}/userHome").deleteDir()
         new File("${FIXTURE_WORKING_DIR}/.gradle").deleteDir()
     }
 
     @Test
-    public void testConfiguration() {
-        Project project = LibProjectFactory.createFromFixture()
-        ReleasePlugin plugin = new ReleasePlugin()
-        plugin.apply(project)
-        project.publish {
-            userOrg = 'novoda'
-            groupId = 'com.example.novoda'
-            artifactId = "test"
-            version = project.version
-            description = 'Super duper easy way to release your Android and other artifacts to bintray'
-            website = "https://github.com/novoda/bintray-release"
-            dryRun = true
-        }
-        project.evaluate()
+    public void testBintrayConfiguration() {
+        assertThat(project.publish.repoName).isEqualTo("maven")
+        assertThat(project.bintray.pkg.repo).isEqualTo("maven")
+        assertThat(project.bintray.pkg.userOrg).isEqualTo("novoda")
+        assertThat(project.bintray.pkg.name).isEqualTo("test")
+    }
 
-        Task task1 = project.getTasks().findByPath(":bintrayUpload")
-        assertThat(task1).isNotNull()
-        Task task2 = project.getTasks().findByPath(":generatePomFileForMavenPublication")
-        assertThat(task2).isNotNull()
+    @Test
+    public void testGeneratedPomFileForMavenPublicationTask() {
 
-        // Now assert some other stuff about the tasks
-        BintrayUploadTask uploadTask = task1 as BintrayUploadTask
+        Task task = project.getTasks().findByPath(":generatePomFileForMavenPublication")
+        assertThat(task).isNotNull()
 
-        GenerateMavenPom generatePomTask = task2 as GenerateMavenPom
+        GenerateMavenPom generatePomTask = task as GenerateMavenPom
         generatePomTask.execute()
         File pomFile = new File(project.buildDir, "/publications/maven/pom-default.xml")
         assertThat(pomFile.exists())
@@ -58,7 +71,7 @@ class TestBintrayRelease {
         // Skip the first two since they're boilerplate xml stuff
         Node groupIdNode = nodes[2]
         assertThat(groupIdNode.name().localPart).isEqualTo "groupId"
-        assertThat(groupIdNode.value()[0]).isEqualTo "com.example.novoda"
+        assertThat(groupIdNode.value()[0]).isEqualTo "com.novoda.demo"
         Node artifactIdNode = nodes[3]
         assertThat(artifactIdNode.name().localPart).isEqualTo "artifactId"
         assertThat(artifactIdNode.value()[0]).isEqualTo "test"
@@ -68,9 +81,21 @@ class TestBintrayRelease {
         Node packagingNode = nodes[5]
         assertThat(packagingNode.name().localPart).isEqualTo "packaging"
         assertThat(packagingNode.value()[0]).isEqualTo "aar"
+    }
 
-        // uploadTask verification here
+    @Test
+    public void testBintrayUploadTask() {
+        Task task = project.getTasks().findByPath(":bintrayUpload")
+        assertThat(task).isNotNull()
 
-        project.buildDir.deleteDir()
+        BintrayUploadTask uploadTask = task as BintrayUploadTask
+        uploadTask.user = "user"
+        uploadTask.apiKey = "apiKey"
+
+        uploadTask.repoName = "maven"
+        uploadTask.packageName = "test"
+        uploadTask.executeWithoutThrowingTaskFailure()
+
+        assertThat(uploadTask.dryRun).isFalse()
     }
 }
