9a6c79b64a7691de6e36fa89f644e8ebb7356c43
Remco Mokveld <remco@thecapitals.nl>
2016-01-27 09:23:50 +0100

    Update unit tests to test creation of variant publications

--- core/src/test/groovy/com/novoda/gradle/release/LibProjectFactory.groovy
+++ core/src/test/groovy/com/novoda/gradle/release/LibProjectFactory.groovy
@@ -24,4 +24,30 @@ final class LibProjectFactory {
 
         return project
     }
+
+    public static Project createFromFixtureWithFlavors() {
+        Project project = ProjectBuilder.builder().withProjectDir(new File(TestConfiguration.FIXTURE_WORKING_DIR)).build()
+        project.apply plugin: 'com.android.library'
+        project.version = "1.0"
+        project.group = "com.novoda.demo.gradle.release"
+        project.android {
+            compileSdkVersion 23
+            buildToolsVersion '23.0.0'
+
+            defaultConfig {
+                versionCode 1
+                versionName '1.0'
+                minSdkVersion 23
+                targetSdkVersion 23
+            }
+
+            productFlavors {
+                flavor1{}
+                flavor2{}
+            }
+        }
+
+        return project
+
+    }
 }
