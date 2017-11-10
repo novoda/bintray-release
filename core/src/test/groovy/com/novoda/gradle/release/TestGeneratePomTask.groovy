71601435ba2e45587bc2eaea59bf4ec913c1c170
Amornchai Kanokpullwad <zoonref@gmail.com>
2017-11-10 12:52:55 +0900

    add android lib testcase

--- core/src/test/groovy/com/novoda/gradle/release/TestGeneratePomTask.groovy
+++ core/src/test/groovy/com/novoda/gradle/release/TestGeneratePomTask.groovy
@@ -11,7 +11,7 @@ import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 class TestGeneratePomTask {
 
     @Rule public TemporaryFolder testProjectDir = new TemporaryFolder()
-    private List<File> pluginClasspath
+    private String pluginClasspath
     private File buildFile
 
     @Before
@@ -21,19 +21,26 @@ class TestGeneratePomTask {
             if (pluginClasspathResource == null) {
                 throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
             }
-            pluginClasspath = pluginClasspathResource.readLines().collect { new File(it) }
-
-            buildFile = testProjectDir.newFile('build.gradle')
+            def classpath = pluginClasspathResource.readLines().collect { new File(it) }
+            pluginClasspath = classpath
+                .collect { it.absolutePath.replace('\\', '\\\\') } // escape backslashes in Windows paths
+                .collect { "'$it'" }
+                .join(", ")
         }
+        buildFile = testProjectDir.newFile('build.gradle')
     }
 
     @Test
-    void testGeneratePomTask() {
+    void testGeneratePomTaskForJavaLib() {
         buildFile << """
-            plugins {
-                id 'java-library'
-                id 'bintray-release'
+            buildscript {
+                dependencies {
+                    classpath files($pluginClasspath)
+                }
             }
+            
+            apply plugin: "java-library"
+            apply plugin: "bintray-release"
             
             publish {
                 userOrg = 'novoda'
@@ -52,10 +59,8 @@ class TestGeneratePomTask {
         def result = GradleRunner.create()
             .withProjectDir(testProjectDir.root)
             .withArguments("generatePomFileForMavenPublication")
-            .withPluginClasspath(pluginClasspath)
             .build()
 
-        print(testProjectDir)
         assert result.task(":generatePomFileForMavenPublication").outcome == SUCCESS
 
         File pomFile = new File(testProjectDir.root, '/build/publications/maven/pom-default.xml')
@@ -66,5 +71,67 @@ class TestGeneratePomTask {
         assert dependencies.find { dep -> dep.artifactId == "hello" && dep.scope == "compile" } != null
         assert dependencies.find { dep -> dep.artifactId == "haha" && dep.scope == "compile" } != null
         assert dependencies.find { dep -> dep.artifactId == "world" && dep.scope == "runtime" } != null
+    }
+
+    @Test
+    void testGeneratePomTaskForAndroidLibrary() {
+        File manifestFile = new File(testProjectDir.root, "/src/main/AndroidManifest.xml")
+        manifestFile.getParentFile().mkdirs()
+        manifestFile.createNewFile()
+        manifestFile << """
+            <manifest package="com.novoda.test"/>
+        """
+
+        buildFile << """
+            buildscript {
+                repositories {
+                    jcenter()
+                    google()
+                }
+                dependencies {
+                    classpath files($pluginClasspath)
+                    classpath 'com.android.tools.build:gradle:3.0.0'
+                }
+            }
+            apply plugin: "com.android.library"
+            apply plugin: "bintray-release"
+            
+            android {
+                compileSdkVersion 26
+                buildToolsVersion "26.0.2"
+
+                defaultConfig {
+                    minSdkVersion 16
+                    versionCode 1
+                    versionName "0.0.1"
+                }    
+            }
+            
+            publish {
+                userOrg = 'novoda'
+                groupId = 'com.novoda'
+                artifactId = 'test'
+                publishVersion = '1.0'
+            }
+        
+            dependencies {
+                compile 'com.abc:hello:1.0.0'
+                implementation 'com.xyz:world:2.0.0'
+                api 'com.xxx:haha:3.0.0'
+            }
+        """
+
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments("generatePomFileForReleasePublication")
+            .build()
+
+        assert result.task(":generatePomFileForReleasePublication").outcome == SUCCESS
+
+        File pomFile = new File(testProjectDir.root, '/build/publications/release/pom-default.xml')
+        def nodes = new XmlSlurper().parse(pomFile)
+        def dependencies = nodes.dependencies.dependency
+
+        assert dependencies.size() == 3
     }
 }
