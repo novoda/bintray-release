d2d8f6fe039c2b98d04051828c1b904cdaa0f989
StefMa <StefMaDev@outlook.com>
2018-01-31 16:12:43 +0100

    Introduce prBuilder shell script

---
+++ ci/prBuilder.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+
+# Testing the core plugin
+cd $BASEDIR/../ && ./gradlew clean build bintrayUpload -PdryRun=true --info
+
+# Testing the samples
+cd $BASEDIR/samples/ && ./gradlew clean build bintrayUpload -PdryRun=true --info
