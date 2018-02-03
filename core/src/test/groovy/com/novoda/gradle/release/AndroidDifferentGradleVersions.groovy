c78812c5221e99ce55d819f244352c79fa56b7d3
StefMa <StefMaDev@outlook.com>
2018-02-03 16:50:04 +0100

    Use plugin code instead of already published

--- core/src/test/groovy/com/novoda/gradle/release/AndroidDifferentGradleVersions.groovy
+++ core/src/test/groovy/com/novoda/gradle/release/AndroidDifferentGradleVersions.groovy
@@ -38,8 +38,11 @@ class AndroidDifferentGradleVersions {
                 }
                 dependencies {
                     classpath 'com.android.tools.build:gradle:3.0.0'
-                    classpath 'com.novoda:bintray-release:0.8.0'
                 }
+            }
+            
+            plugins {
+                id 'com.novoda.bintray-release' apply false
             }
             
             apply plugin: "com.android.library"
