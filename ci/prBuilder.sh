0f7acf8cacb6552b1cd003e5db59c5da09dc5ae8
StefMa <StefMaDev@outlook.com>
2018-02-06 20:44:25 +0100

    Exit script if command not exit with 0

--- ci/prBuilder.sh
+++ ci/prBuilder.sh
@@ -2,8 +2,16 @@
 
 BASEDIR=$(dirname "$0")
 
+function exitIfCommandFailed {
+    if [ $1 -ne 0 ]
+        then exit $1
+    fi
+}
+
 # Testing the core plugin
 cd $BASEDIR/../ && ./gradlew clean build bintrayUpload -PdryRun=true --info
+exitIfCommandFailed $?
 
 # Testing the samples
 cd $BASEDIR/../samples/ && ./gradlew clean build bintrayUpload -PdryRun=true --info
+exitIfCommandFailed $?
