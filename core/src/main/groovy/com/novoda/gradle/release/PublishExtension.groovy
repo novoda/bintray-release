3f76838a9e6390aab2db4b19c7b6fe4882dd6b1e
StefMa <StefMaDev@outlook.com>
2018-02-08 10:59:11 +0100

    Move extension property validation to extension

--- core/src/main/groovy/com/novoda/gradle/release/PublishExtension.groovy
+++ core/src/main/groovy/com/novoda/gradle/release/PublishExtension.groovy
@@ -1,5 +1,7 @@
 package com.novoda.gradle.release
 
+import groovy.transform.PackageScope
+
 class PublishExtension {
 
     String repoName = 'maven'
@@ -29,5 +31,35 @@ class PublishExtension {
     boolean override = false
 
     String[] publications
+
+    /**
+     * Validate all mandatory properties for this extension.
+     *
+     * Will throw a Exception if not setup correctly.
+     */
+    @PackageScope
+    void validate() {
+        String extensionError = "";
+        if (userOrg == null) {
+            extensionError += "Missing userOrg. "
+        }
+        if (groupId == null) {
+            extensionError += "Missing groupId. "
+        }
+        if (artifactId == null) {
+            extensionError += "Missing artifactId. "
+        }
+        if (publishVersion == null) {
+            extensionError += "Missing publishVersion. "
+        }
+        if (desc == null) {
+            extensionError += "Missing desc. "
+        }
+
+        if (extensionError) {
+            String prefix = "Have you created the publish closure? "
+            throw new IllegalStateException(prefix + extensionError)
+        }
+    }
 
 }
