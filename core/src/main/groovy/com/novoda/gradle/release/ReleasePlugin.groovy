fc5952d59033b7510a9a499ed029d51a420aa8de
Antonio Bertucci <archano@gmail.com>
2018-10-04 09:19:59 +0200

    Extract common setup for publications

--- core/src/main/groovy/com/novoda/gradle/release/ReleasePlugin.groovy
+++ core/src/main/groovy/com/novoda/gradle/release/ReleasePlugin.groovy
@@ -3,6 +3,8 @@ package com.novoda.gradle.release
 import com.jfrog.bintray.gradle.BintrayPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ExcludeRule
+import org.gradle.api.artifacts.ModuleDependency
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 
@@ -20,55 +22,56 @@ class ReleasePlugin implements Plugin<Project> {
         }
     }
 
-    void attachArtifacts(PublishExtension extension, Project project) {
-        PublishingExtension mavenPublishingExtension = project.extensions.findByType(PublishingExtension)
-
-        PropertyFinder propertyFinder = new PropertyFinder(project, extension)
+    private void attachArtifacts(PublishExtension extension, Project project) {
         if (project.plugins.hasPlugin('com.android.library')) {
             project.android.libraryVariants.all { variant ->
-                def name = variant.name
-                Artifacts artifacts = new AndroidArtifacts(variant)
-                MavenPublication mavenPublication = mavenPublishingExtension.publications.create(name, MavenPublication) {
-                    groupId extension.groupId
-                    artifactId extension.artifactId
-                    version = propertyFinder.publishVersion
-
-                    artifacts.all(name, project).each {
-                        artifact it
+                MavenPublication publication = createPublication(variant.name, project, extension, new AndroidArtifacts(variant))
+                Set<ModuleDependency> dependenciesSet = new HashSet<>()
+                ['compile', 'implementation', 'api'].each { configurationName ->
+                    project.configurations.findByName(configurationName)?.with { configuration ->
+                        dependenciesSet.addAll(configuration.incoming.dependencies.withType(ModuleDependency))
                     }
+                }
 
-                    pom.withXml {
-                        def dependenciesNode = asNode().appendNode('dependencies')
-                        def configurationContainer = project.configurations
-                        def allDependencies = []
-                        allDependencies.addAll(configurationContainer.findByName('compile')?.allDependencies ?: [])
-                        allDependencies.addAll(configurationContainer.findByName('implementation')?.allDependencies ?: [])
-                        allDependencies.addAll(configurationContainer.findByName('api')?.allDependencies ?: [])
+                publication.pom.withXml {
+                    def dependenciesNode = asNode().appendNode('dependencies')
+                    dependenciesSet.each { ModuleDependency dependency ->
+                        if (dependency.name != 'unspecified') {
+                            def dependencyNode = dependenciesNode.appendNode('dependency')
+                            dependencyNode.appendNode('groupId', dependency.group)
+                            dependencyNode.appendNode('artifactId', dependency.name)
+                            dependencyNode.appendNode('version', dependency.version)
 
-                        allDependencies.each {
-                            if (it.name != 'unspecified') {
-                                def dependencyNode = dependenciesNode.appendNode('dependency')
-                                dependencyNode.appendNode('groupId', it.group)
-                                dependencyNode.appendNode('artifactId', it.name)
-                                dependencyNode.appendNode('version', it.version)
+                            if (!dependency.excludeRules.isEmpty()) {
+                                def exclusions = dependencyNode.appendNode('exclusions')
+                                dependency.excludeRules.each { ExcludeRule excludeRule ->
+                                    def exclusion = exclusions.appendNode('exclusion')
+                                    exclusion.appendNode('groupId', excludeRule.group)
+                                    exclusion.appendNode('artifactId', excludeRule.module)
+                                }
                             }
                         }
                     }
                 }
             }
         } else {
-            Artifacts artifacts = new JavaArtifacts()
-            def name = 'maven'
-            MavenPublication mavenPublication = mavenPublishingExtension.publications.create(name, MavenPublication) {
-                groupId extension.groupId
-                artifactId extension.artifactId
-                version = propertyFinder.publishVersion
-
-                artifacts.all(name, project).each {
-                    artifact it
-                }
-                from project.components.java
-            }
+            MavenPublication publication = createPublication('maven', project, extension, new JavaArtifacts())
+            publication.from project.components.java
         }
     }
+
+    private MavenPublication createPublication(String name, Project project, PublishExtension extension, Artifacts artifacts) {
+        PublishingExtension mavenPublishingExtension = project.extensions.findByType(PublishingExtension)
+        PropertyFinder propertyFinder = new PropertyFinder(project, extension)
+        MavenPublication publication = mavenPublishingExtension.publications.create(name, MavenPublication) {
+            groupId = extension.groupId
+            artifactId = extension.artifactId
+            version = propertyFinder.publishVersion
+        }
+        artifacts.all(publication.name, project).each { artifactSource ->
+            publication.artifact artifactSource
+        }
+        return publication
+    }
+
 }
